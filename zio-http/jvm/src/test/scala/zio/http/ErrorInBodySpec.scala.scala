c5d153760a53d74650a888b0bee1083c77618a76
pfeme <pablofeme@gmail.com>
2024-08-06 00:04:25 +0200

    w

--- zio-http/jvm/src/test/scala/zio/http/ErrorInBodySpec.scala.scala
+++ zio-http/jvm/src/test/scala/zio/http/ErrorInBodySpec.scala.scala
@@ -25,34 +25,22 @@ object ErrorInBodySpec extends ZIOHttpSpec {
       },
       test("include error in body") {
         val routes =
-          Routes(Method.GET / "test" -> Handler.ok.map(_ => throw new Throwable("Error"))).includeErrorDetails
+          Routes(Method.GET / "test2" -> Handler.ok.map(_ => throw new Throwable("Error"))).includeErrorDetails
         assertZIO(for {
           port   <- Server.install(routes)
           client <- ZIO.service[Client]
-          url = URL.decode("http://localhost:%d/%s".format(port, Path.root / "test")).toOption.get
-          body    <- client(Request(url = url)).map(_.body)
-          content <- body.asString
-        } yield content)(not(isEmptyString)) &&
-        assertTrue(routes.routes.map(_.errorState) == Chunk(true))
-      },
-      test("include error in body2") {
-        val routes =
-          Routes((Method.GET / "test" -> Handler.ok.map(_ => throw new Throwable("Error"))).includeErrorDetails)
-        assertZIO(for {
-          port   <- Server.install(routes)
-          client <- ZIO.service[Client]
-          url = URL.decode("http://localhost:%d/%s".format(port, Path.root / "test")).toOption.get
+          url = URL.decode("http://localhost:%d/%s".format(port, Path.root / "test2")).toOption.get
           body    <- client(Request(url = url)).map(_.body)
           content <- body.asString
         } yield content)(not(isEmptyString)) &&
         assertTrue(routes.routes.map(_.errorState) == Chunk(true))
       },
       test("exclude error in body") {
-        val routes = Routes(Method.GET / "test" -> Handler.ok.map(_ => throw new Throwable("Error")))
+        val routes = Routes(Method.GET / "test3" -> Handler.ok.map(_ => throw new Throwable("Error")))
         assertZIO(for {
           port   <- Server.install(routes.includeErrorDetails.excludeErrorDetails)
           client <- ZIO.service[Client]
-          url = URL.decode("http://localhost:%d/%s".format(port, Path.root / "test")).toOption.get
+          url = URL.decode("http://localhost:%d/%s".format(port, Path.root / "test3")).toOption.get
           body    <- client(Request(url = url)).map(_.body)
           content <- body.asString
         } yield content)(isEmptyString) &&
